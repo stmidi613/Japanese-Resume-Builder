498c5f8ed67fff9160601843290f54717380f3b7
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-04-15 18:46:05 +0900

    I finished the first form section except for the next button.  I made some changes to the css from the figma design for convenience and better code.  The figma design was a little sloppy regarding the cell phone part.

--- src/Components/Resume/Form1.tsx
+++ src/Components/Resume/Form1.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import photo from "../../images/photo.png";
 import UploadButton from "../Buttons/UploadButton";
 import TipsIcon from "./TipsIcon";
-import "./resume.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./resume.css";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 
 const Form1: React.FC = () => {
   return (
@@ -12,9 +14,7 @@ const Form1: React.FC = () => {
         <section className="section">
           <div className="mb-1 flex justify-center">
             <TipsIcon />
-            <h2 className="h2">
-              Photo
-            </h2>
+            <h2 className="h2">Photo</h2>
           </div>
           <div className="flex w-48 flex-col justify-center">
             <img src={photo} alt="photo" />
@@ -25,14 +25,145 @@ const Form1: React.FC = () => {
         <section className="section">
           <div className="section-div">
             <TipsIcon />
-            <h2 className="h2">
-              Name
-            </h2>
+            <h2 className="h2">Name</h2>
           </div>
           <div className="input-div">
-            <input className="input focus:outline-none" placeholder="Name Furigana" type="text" name="fname" aria-required />
-            <input className="input focus:outline-none mt-2" placeholder="Full Name" type="text" name="fname" aria-required />
+            <input
+              className="input focus:outline-none"
+              placeholder="Name Furigana"
+              type="text"
+              aria-required
+            />
+            <input
+              className="input focus:outline-none mt-2"
+              placeholder="Full Name"
+              type="text"
+              aria-required
+            />
           </div>
+        </section>
+        {/* Date of Birth Section */}
+        <section className="section">
+          <div className="section-div">
+            <TipsIcon />
+            <h2 className="h2">Date of Birth</h2>
+          </div>
+          <div className="input-div">
+            <input
+              className="input focus:outline-none"
+              type="date"
+              aria-required
+            />
+            <FontAwesomeIcon
+              id="date"
+              className="relative bottom-6 left-2 text-purple"
+              icon={faCalendarDays}
+            />
+          </div>
+        </section>
+        {/* Address Section */}
+        <section className="section">
+          <div className="section-div">
+            <TipsIcon />
+            <h2 className="h2">Address</h2>
+          </div>
+          <div className="input-div">
+            <input
+              className="input focus:outline-none"
+              placeholder="Address Furigana"
+              type="text"
+              aria-required
+            />
+            <input
+              className="input focus:outline-none mt-2"
+              placeholder="Address"
+              type="text"
+              aria-required
+            />
+          </div>
+        </section>
+        {/* Email Section */}
+        <section className="section">
+          <div className="section-div">
+            <TipsIcon />
+            <h2 className="h2">Email</h2>
+          </div>
+          <div className="input-div">
+            <input
+              className="input focus:outline-none"
+              placeholder="Address Furigana"
+              type="email"
+              aria-required
+            />
+          </div>
+        </section>
+        {/* Gender Section */}
+        <section className="flex section">
+          <div className="section-div">
+            <TipsIcon />
+            <h2 className="h2">Gender</h2>
+          </div>
+          <div className="flex gap-6">
+            <div>
+              <input className="input mr-2" type="radio" name="gender" value="Male" />
+              <label className="text-purple" htmlFor="male">Male</label>
+            </div>
+            <div>
+              <input className="input mr-2" type="radio" name="gender" value="Female" />
+              <label className="text-purple" htmlFor="female">Female</label>
+            </div>
+          </div>
+        </section>
+        {/* Phone Section */}
+        <section className="section">
+          <div className="section-div">
+            <TipsIcon />
+            <h2 className="h2">Home Phone</h2>
+          </div>
+          <div className="input-div">
+            <input
+              className="input focus:outline-none"
+              placeholder="Home Phone"
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+              aria-required
+            />
+            <input
+              className="input focus:outline-none mt-2"
+              placeholder="Cell Phone"
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+              aria-required
+            />
+          </div>
+        </section>
+        {/* Contact's Information Section */}
+        <section className="section">
+          <div className="section-div">
+            <TipsIcon />
+            <h2 className="h2">Contact's Information</h2>
+          </div>
+          <div className="input-div">
+            <input
+              className="input focus:outline-none"
+              placeholder="Contact's Address Furigana"
+              type="text"
+              aria-required
+            />
+            <input
+              className="input focus:outline-none mt-2"
+              placeholder="Contact's Address"
+              type="text"
+              aria-required
+            />
+          </div>
+          <input
+              className="input focus:outline-none mt-2"
+              placeholder="Contact's Phone Number"
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+              aria-required
+            />
         </section>
       </form>
     </>
