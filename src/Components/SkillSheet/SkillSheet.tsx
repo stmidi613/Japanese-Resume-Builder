f9c209dad37e5ff3be98a3fc9e04abfb92a4a0b5
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-06-08 02:29:22 +0900

    I continued the skill sheet and still need to finish part 2 and 3.  Then I will create a new css sheet and clean the code.

--- src/Components/SkillSheet/SkillSheet.tsx
+++ src/Components/SkillSheet/SkillSheet.tsx
@@ -32,24 +32,77 @@ const SkillSheet: React.FC = () => {
           <section className="page-div">
             <table className="grid grid-flow-col grid-cols-12 w-full border-[1px] border-b-[0px]">
               <tr className="bg-skillsheetgray grid col-span-2 text-sm border-r-[1px]">
-                <td className="grid place-content-center p-1 border-b-[1px]">ふりがな</td>
-                <td className="grid place-content-center p-1 border-b-[1px]">氏名</td>
-                <td className="grid place-content-center p-1 border-b-[1px]">事務要約</td>
-                <td className="grid place-content-center p-1 border-b-[1px]">活かせる経験・知識</td>
+                <td className="grid place-content-center p-1 border-b-[1px] h-8">
+                  ふりがな
+                </td>
+                <td className="grid place-content-center p-1 border-b-[1px] h-8">
+                  氏名
+                </td>
+                <td className="grid place-content-center p-1 border-b-[1px] h-12">
+                  事務要約
+                </td>
+                <td className="grid place-content-center p-1 border-b-[1px] h-16">
+                  活かせる経験・知識
+                </td>
               </tr>
               <tr className="grid col-span-6">
-                <td className="grid place-content-center p-1 border-b-[1px]"></td>
-                <td className="grid place-content-center p-1 border-b-[1px]"></td>
-                <td className="grid place-content-center p-1 border-b-[1px]"></td>
-                <td className="grid place-content-center p-1 border-b-[1px]"></td>
+                <td className="grid content-center p-1 border-b-[1px] h-8"></td>
+                <td className="grid content-center p-1 border-b-[1px] h-8"></td>
+                <td className="grid content-center p-1 border-b-[1px] h-12"></td>
+                <td className="grid content-center p-1 border-b-[1px] h-16"></td>
               </tr>
-              <tr className="bg-skillsheetgray grid col-span-2 border-x-[1px]">
-                <td className="grid place-content-center p-1 border-b-[1px]">保有技術</td>
-                <td className="grid place-content-center p-1 border-b-[1px]">その他</td>
+              <tr className="bg-skillsheetgray grid col-span-1 border-x-[1px]">
+                <td className="grid place-content-center text-sm p-1 border-b-[1px]">
+                  保有技術
+                </td>
+                <td className="grid place-content-center text-sm p-1 border-b-[1px]">
+                  その他
+                </td>
+              </tr>
+              <tr className="grid col-span-3">
+                <td className="grid content-center p-1 border-b-[1px]"></td>
+                <td className="grid content-center p-1 border-b-[1px]"></td>
+              </tr>
+            </table>
+            {/* 経験プロジェクト */}
+            <table className="mt-5 grid grid-flow-col grid-cols-12 w-full border-[1px] border-b-[0px]">
+            <tr className="bg-skillsheetgray grid col-span-2 border-r-[1px]">
+                <td className="grid place-content-center p-1">
+                  勤務先
+                </td>
               </tr>
               <tr className="grid col-span-2">
-                <td className="grid place-content-center p-1 border-b-[1px]"></td>
-                <td className="grid place-content-center p-1 border-b-[1px]"></td>
+                <td className="grid content-center p-1"></td>
+              </tr>
+            </table>
+            <table className="grid grid-flow-col grid-cols-12 w-full border-[1px] border-b-[0px]">
+              <tr className="grid col-span-2 text-sm border-r-[1px]">
+                <td className="bg-skillsheetgray grid place-content-center p-1 border-b-[1px] h-8">
+                  案件名
+                </td>
+                <td className="grid place-content-center p-1 border-b-[1px] h-56">
+                 
+                </td>
+              </tr>
+              <tr className="grid col-span-6">
+                <td className="grid content-center p-1 border-b-[1px] h-8"></td>
+                <td className="grid content-center p-1 border-b-[1px] h-56"></td>
+              </tr>
+              <tr className="grid col-span-1 border-x-[1px]">
+                <td className="bg-skillsheetgray text-sm grid place-content-center p-1 border-b-[1px] h-8">
+                  環境・言語
+                </td>
+                <td className="grid place-content-center p-1 border-b-[1px]">
+                </td>
+              </tr>
+              <tr className="grid col-span-3 h-56">
+                <td className="bg-skillsheetgray grid place-content-center p-1 border-b-[1px] h-8">役割・役職</td>
+                <td className="grid content-center p-1 border-b-[1px] h-12"></td>
+                <td className="grid bg-skillsheetgray place-content-center p-1 border-b-[1px] h-8">規模・人数</td>
+                <td className="grid content-center p-1 border-b-[1px] h-12"></td>
+                <td className="grid bg-skillsheetgray place-content-center p-1 border-b-[1px] h-8">担当工程</td>
+                <td className="grid bg-skillsheetgray content-center p-1 border-b-[1px] h-8"></td>
+                <td className="grid content-center p-1 border-b-[1px] h-8"></td>
               </tr>
             </table>
           </section>
