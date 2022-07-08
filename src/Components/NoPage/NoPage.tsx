4409d5133e6e600badbbf13b09cb6b8db5493014
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2022-07-08 20:32:56 +0900

    changed the css for the noPageFound component.

--- src/Components/NoPage/NoPage.tsx
+++ src/Components/NoPage/NoPage.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const NoPage: React.FC = () => {
   return (
-    <div className='grid place-content-center text-5xl text-purple'>No Page Found</div>
-  )
-}
+    <>
+      <div className="p-10 w-full text-5xl text-purple">
+        <p className="grid place-content-center">No Page Found</p>
+      </div>
+    </>
+  );
+};
 
-export default NoPage
+export default NoPage;
